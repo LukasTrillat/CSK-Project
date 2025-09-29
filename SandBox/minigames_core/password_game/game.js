5d7eaae48463e68dd651ed8c55e597ac3204c3ff
Lukas Trillat <lstrillat@gmail.com>
2025-09-29 10:57:50 -0300

    core game updated

--- SandBox/minigames_core/password_game/game.js
+++ SandBox/minigames_core/password_game/game.js
@@ -1,47 +1,54 @@
 
-const buttons = {}
+const letterTiles = {}
+const letterTileCanvases = ["tile_1", "tile_2", "tile_3", "tile_4"]
 
-function createButton() {}
 
-buttons.test_button = new rive.Rive({
-    src: "animations/rive_test_button.riv",
-    canvas: document.getElementById("test_button_canvas"),
-    stateMachines: "STATE-MACHINE",
 
-    onLoad: () => {
-        buttons.test_button.resizeDrawingSurfaceToCanvas();
+function renderTile(buttonName,sourceFile, canvasId, stateMachine = "STATE_MACHINE") {
+    const instance = new rive.Rive({
+        src: sourceFile,
+        canvas: document.getElementById(canvasId),
+        stateMachines: stateMachine,
+
+        onLoad: () => {instance.resizeDrawingSurfaceToCanvas()}
+    })
+
+    letterTiles[buttonName] = instance
+}
+
+
+
+letterTileCanvases.forEach(function(tileName){
+    renderTile(buttonName=tileName, sourceFile="animations/rive_test_button.riv",canvasId=tileName)
+
+    let newX = 0, newY = 0, startX = 0, startY = 0;
+
+    const card = document.getElementById(tileName)
+
+    card.addEventListener('mousedown', mouseDown)
+
+    function mouseDown(e){
+        startX = e.clientX
+        startY = e.clientY
+
+        document.addEventListener('mousemove', mouseMove)
+        document.addEventListener('mouseup', mouseUp)
     }
+
+    function mouseMove(e){
+        newX = startX - e.clientX 
+        newY = startY - e.clientY 
+    
+        startX = e.clientX
+        startY = e.clientY
+
+        card.style.top = (card.offsetTop - newY) + 'px'
+        card.style.left = (card.offsetLeft - newX) + 'px'
+    }
+
+    function mouseUp(e){
+        document.removeEventListener('mousemove', mouseMove)
+    }
+
 })
 
-
-
-            // MOVING LETTER TILES //
-
-let newX = 0, newY = 0, startX = 0, startY = 0;
-
-const letterTile = document.getElementById('test_button_canvas')
-
-letterTile.addEventListener('mousedown', mouseDown)
-
-function mouseDown(e){
-    startX = e.clientX
-    startY = e.clientY
-
-    document.addEventListener('mousemove', mouseMove)
-    document.addEventListener('mouseup', mouseUp)
-}
-
-function mouseMove(e){
-    newX = startX - e.clientX 
-    newY = startY - e.clientY 
-  
-    startX = e.clientX
-    startY = e.clientY
-
-    letterTile.style.top = (letterTile.offsetTop - newY) + 'px'
-    letterTile.style.left = (letterTile.offsetLeft - newX) + 'px'
-}
-
-function mouseUp(e){
-    document.removeEventListener('mousemove', mouseMove)
-}
