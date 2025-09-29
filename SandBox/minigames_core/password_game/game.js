ef6b9cf9d759aa8387753574afb8e5504e32347c
Lukas Trillat <lstrillat@gmail.com>
2025-09-29 08:56:56 -0300

    fix typo

--- SandBox/minigames_core/password_game/game.js
+++ SandBox/minigames_core/password_game/game.js
@@ -1,7 +1,7 @@
 const animations = {}
 
 const rive_bar_arrow = new rive.Rive({
-    src: "rive_bar_arrow.riv",
+    src: "animations/rive_bar_arrow.riv",
     canvas: document.getElementById("bar_arrow_canvas"),
     stateMachines: "STATE-MACHINE",
 
