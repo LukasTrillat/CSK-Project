a0ab1ac4cf1f63ceadd370cc83426c48a8b7c7c0
Lukas Trillat <lstrillat@gmail.com>
2025-09-29 08:54:52 -0300

    first minigame core

---
+++ SandBox/minigames_core/password_game/game.js
@@ -0,0 +1,21 @@
+const animations = {}
+
+const rive_bar_arrow = new rive.Rive({
+    src: "rive_bar_arrow.riv",
+    canvas: document.getElementById("bar_arrow_canvas"),
+    stateMachines: "STATE-MACHINE",
+
+    onLoad: () => {
+        rive_bar_arrow.resizeDrawingSurfaceToCanvas();
+    }
+})
+
+const rive_bar_goal = new rive.Rive({
+    src: "animations/rive_bar_goal.riv",
+    canvas: document.getElementById("bar_goal_canvas"),
+    stateMachines: "STATE-MACHINE",
+
+    onLoad: () => {
+        rive_bar_goal.resizeDrawingSurfaceToCanvas();
+    }
+})
