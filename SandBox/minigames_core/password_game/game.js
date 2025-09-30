54f64afef30be133d2d581ead318ad01d10fe6a0
Lukas Trillat <lstrillat@gmail.com>
2025-09-30 16:27:11 -0300

    Word generation algorithm and tile rendering

--- SandBox/minigames_core/password_game/game.js
+++ SandBox/minigames_core/password_game/game.js
@@ -1,27 +1,28 @@
 
-const letterTiles = {}
-const letterTileCanvases = []
+// ------ ALGORITHM TO GENERATE CHARACTERS  ------ // 
+
 const all_characters = "abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ1234567890!?¿.,_-".split('')
 const uppercase_letters = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
 const letters = "abcdefghijklmnñopqrstuvwxyz".split('')
 const special_characters = "!?¿.,_-".split('')
 const numbers = "1234567890".split('')
-
-function generateTileSet(total=25, numLetters=10, numSpecial_chars=4, numUppercases=6, numNumbers=5){
+function generateLetters(total=25, numLetters=10, numSpecial_chars=4, numUppercases=6, numNumbers=5){
     let finalList = []
     for (i=0;i<numLetters;i++){ finalList.push(letters[Math.floor(Math.random() * letters.length)]) }
     for (i=0;i<numSpecial_chars;i++){ finalList.push(special_characters[Math.floor(Math.random() * special_characters.length)]) }
     for (i=0;i<numUppercases;i++){ finalList.push(uppercase_letters[Math.floor(Math.random() * uppercase_letters.length)]) }
     for (i=0;i<numNumbers;i++){ finalList.push(numbers[Math.floor(Math.random() * numbers.length)]) }
-    console.log(finalList);
     finalList.sort(() => Math.random() - 0.5);
     return finalList
 }
 
-generateTileSet()
+// ------ RENDER LETTER TILES ------ // 
 
 
-// Function to render the tiles
+const letterTiles = {}
+const letterTileCanvases_ID = []
+const letterTileContainers_ID = []
+
 function renderTile(buttonName,sourceFile, canvasId, stateMachine = "STATE_MACHINE") {
     const instance = new rive.Rive({
         src: sourceFile,
@@ -33,30 +34,62 @@ function renderTile(buttonName,sourceFile, canvasId, stateMachine = "STATE_MACHI
 }
 
 //Creating the canvases
-ammount_tiles = 20;
+ammount_tiles = 25;
+tile_canvas_size = 128
+char_list = generateLetters()
+console.log(char_list)
+
 for (i= 1; i< ammount_tiles + 1; i ++)
 {
-    letterTileCanvases.push("tile_"+i)
-    let canvas = document.createElement("canvas")
-    canvas.id = "tile_"+i;
-    document.body.appendChild(canvas)
+    const container = document.createElement("div")
+    container.id = "tile_container"+i;
+    container.style.position = "absolute";
+    container.style.top = "60px";
+    container.style.left = 80 * i + "px";
+
+    //Creates a separated canvas to render the letter
+    const letter_canvas = document.createElement("canvas")
+    letter_canvas.width = tile_canvas_size;
+    letter_canvas.height = tile_canvas_size;
+    letter_canvas.style.position = "absolute";
+    letter_canvas.style.top = "0px";
+    letter_canvas.style.left = "40px";
+    
+
+        //draw the text itself
+        const ctx = letter_canvas.getContext("2d")
+        ctx.font = "50px Arial";
+        ctx.fillText(char_list[i],10,80)
+
+    //Creates a canvas to render the tile, assign it the ID and fix size
+    const rive_canvas = document.createElement("canvas")
+    rive_canvas.id = "tile_"+i;
+    rive_canvas.width = tile_canvas_size;
+    rive_canvas.height = tile_canvas_size;
+
+    container.appendChild(rive_canvas)
+    container.appendChild(letter_canvas)
+    document.body.appendChild(container)
+
+    //Adds the canvas to the list
+    letterTileCanvases_ID.push("tile_"+i)
+    letterTileContainers_ID.push("tile_container"+i)
 }
 
+// For each canvas, render a tile
+letterTileCanvases_ID.forEach(function(tile_id){
+    renderTile(buttonName=tile_id, sourceFile="animations/rive_test_button.riv",canvasId=tile_id)})
 
-// For each canvas, create a tile and give it movement
-letterTileCanvases.forEach(function(tileName){
-    renderTile(buttonName=tileName, sourceFile="animations/rive_test_button.riv",canvasId=tileName)
-
+// For each container, give it movement
+letterTileContainers_ID.forEach(function(container_id){
     let newX = 0, newY = 0, startX = 0, startY = 0;
 
-    const card = document.getElementById(tileName)
-
-    card.addEventListener('mousedown', mouseDown)
+    const container = document.getElementById(container_id)
+    container.addEventListener('mousedown', mouseDown)
 
     function mouseDown(e){
         startX = e.clientX
         startY = e.clientY
-
         document.addEventListener('mousemove', mouseMove)
         document.addEventListener('mouseup', mouseUp)
     }
@@ -68,13 +101,21 @@ letterTileCanvases.forEach(function(tileName){
         startX = e.clientX
         startY = e.clientY
 
-        card.style.top = (card.offsetTop - newY) + 'px'
-        card.style.left = (card.offsetLeft - newX) + 'px'
+        container.style.top = (container.offsetTop - newY) + 'px'
+        container.style.left = (container.offsetLeft - newX) + 'px'
     }
 
     function mouseUp(e){
         document.removeEventListener('mousemove', mouseMove)
     }
-
 })
+
+
+
+
+
+
+// ------ PROGRESS BAR  ------ // 
+
+
 
