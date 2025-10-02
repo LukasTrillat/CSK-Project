5b95a52fe48ab94181de6f2035ce24ff7af63208
Lukas Trillat <lstrillat@gmail.com>
2025-10-02 20:27:26 -0300

    just making shure everything is up to date

--- SandBox/minigames_core/password_game/game copy.js
+++ SandBox/minigames_core/password_game/game copy.js
@@ -6,7 +6,7 @@ const uppercase_letters = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
 const letters = "abcdefghijklmnñopqrstuvwxyz".split('')
 const special_characters = "!?¿.,_-".split('')
 const numbers = "1234567890".split('')
-function generateLetters(total=26, numLetters=11, numSpecial_chars=4, numUppercases=6, numNumbers=5){
+function generateLetters(total=25, numLetters=10, numSpecial_chars=4, numUppercases=6, numNumbers=5){
     let finalList = []
     for (i=0;i<numLetters;i++){ finalList.push(letters[Math.floor(Math.random() * letters.length)]) }
     for (i=0;i<numSpecial_chars;i++){ finalList.push(special_characters[Math.floor(Math.random() * special_characters.length)]) }
@@ -18,11 +18,11 @@ function generateLetters(total=26, numLetters=11, numSpecial_chars=4, numUpperca
 
 // ------ RENDER LETTER TILES ------ // 
 
+
 const letterTiles = {}
 const letterTileCanvases_ID = []
 const letterTileContainers_ID = []
 
-// Function to import Rive Files
 function renderTile(buttonName,sourceFile, canvasId, stateMachine = "STATE_MACHINE") {
     const instance = new rive.Rive({
         src: sourceFile,
@@ -34,49 +34,32 @@ function renderTile(buttonName,sourceFile, canvasId, stateMachine = "STATE_MACHI
 }
 
 //Creating the canvases
-ammount_tiles = 26;
-tile_canvas_size = 100
-char_list = generateLetters()
-console.log(char_list)
+ammount_tiles = 1;
+tile_canvas_size = 128
 for (i= 1; i< ammount_tiles + 1; i ++)
 {
-    // -- CANVAS CONTAINER ( RIVE - TEXT) --
     const container = document.createElement("div")
     container.id = "tile_container"+i;
-    container.style.position = "absolute";
 
-    //After 14 tiles, render them a bit lower
-    if (i < 14)
-    {
-        container.style.top = "660px";
-        container.style.left = 80 * i + "px";
-    }
-    else {
-        container.style.top = "800px";
-        container.style.left = 80 * (i -15) + "px";
-    }
-
-    // -- LETTER CANVAS --
+    //Creates a separated canvas to render the letter
     const letter_canvas = document.createElement("canvas")
     letter_canvas.width = tile_canvas_size;
     letter_canvas.height = tile_canvas_size;
     letter_canvas.style.position = "absolute";
-    letter_canvas.style.top = "0px";
-    letter_canvas.style.left = "0px";
-        //Draws the letter itself
-        const ctx = letter_canvas.getContext("2d")
-        ctx.font = "40px Cherokee Mystique";
-        ctx.textAlign = "center";  
-        ctx.textBaseline = "middle";
-        ctx.fillText(char_list[i - 1],tile_canvas_size / 2, tile_canvas_size / 2)
+    letter_canvas.style.top = "0";
+    letter_canvas.style.left = "0";
 
-    // -- RIVE CANVAS --
+        //draw the text itself
+        const ctx = letter_canvas.getContext("2d")
+        ctx.font = "50px Arial";
+        ctx.fillText("A",10,80)
+
+    //Creates a canvas to render the tile, assign it the ID and fix size
     const rive_canvas = document.createElement("canvas")
     rive_canvas.id = "tile_"+i;
     rive_canvas.width = tile_canvas_size;
     rive_canvas.height = tile_canvas_size;
 
-    // -- APPEND CANVASES TO HTML AND LIST --
     container.appendChild(rive_canvas)
     container.appendChild(letter_canvas)
     document.body.appendChild(container)
@@ -86,11 +69,11 @@ for (i= 1; i< ammount_tiles + 1; i ++)
     letterTileContainers_ID.push("tile_container"+i)
 }
 
-// For each canvas, render a tile
+// For each canvas, create a tile and give it movement
 letterTileCanvases_ID.forEach(function(tile_id){
     renderTile(buttonName=tile_id, sourceFile="animations/rive_test_button.riv",canvasId=tile_id)})
 
-// Give movement to each letter container
+
 letterTileContainers_ID.forEach(function(container_id){
     let newX = 0, newY = 0, startX = 0, startY = 0;
 
@@ -121,7 +104,11 @@ letterTileContainers_ID.forEach(function(container_id){
 })
 
 
-// ------ PROGRESS BAR LOGIC AND RENDERING ------ // 
+
+
+
+
+// ------ PROGRESS BAR  ------ // 
 
 
 
